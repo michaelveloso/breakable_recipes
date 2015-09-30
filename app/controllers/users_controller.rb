ca72faecb2439fb06c07a95ff958a7eee8fcd080
michaelveloso <mjv.audio@gmail.com>
2015-09-30 12:51:09 -0400

    user show spec complete for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action confirm_user
 
   def show
+    confirm_user
     @user = User.find(params[:id])
   end
 
   private
 
   def confirm_user
-    if current_user != @user
+    if current_user != User.find(params[:id])
       flash[:errors] = "You're not signed in as this user"
       redirect_to root_path
     end
---
+++ spec/features/user/user_show_page_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+feature 'user visits account page', %Q{
+  As a signed up user
+  I want see my information
+  So I can verify that it's correct
+
+  Acceptance criteria:
+
+  [] - User can visit their account page by clicking 'My Account' if signed in
+  [] - User sees their information on their account page
+  [] - Users can't see other users' account pages
+} do
+  context 'user accesses own account' do
+
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+
+      visit new_user_session_path
+
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+
+      click_button 'Log in'
+
+      click_link 'My Account'
+    end
+
+    scenario 'user can see their account page by clicking "My Account"' do
+      expect(current_path).to eq(user_path(@user))
+    end
+
+    scenario 'user sees their information on their account page' do
+      expect(page).to have_content(@user.email)
+      expect(page).to have_content(@user.full_name)
+      expect(page).to have_content(@user.username)
+    end
+  end
+
+  scenario 'user can\'t access another user\'s account' do
+    user = FactoryGirl.create(:user)
+    bad_user = FactoryGirl.create(:user)
+    visit new_user_session_path
+
+    fill_in 'Email', with: bad_user.email
+    fill_in 'Password', with: bad_user.password
+    click_button 'Log in'
+
+    visit user_path(user)
+    expect(page).to have_content("You're not signed in as this user")
+    expect(current_path).to eq(root_path)
+  end
+end
