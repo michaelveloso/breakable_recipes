001cb50bbc5447a821d91f696935a352cd0796c1
michaelveloso <mjv.audio@gmail.com>
2015-09-30 09:08:41 -0400

    tweaks to user sign in

--- spec/features/user/user_signs_in_spec.rb
+++ spec/features/user/user_signs_in_spec.rb
@@ -4,19 +4,33 @@ feature 'user signs in', %Q{
   As a signed up user
   I want to sign in
   So that I can regain access to my account
+
+  Acceptance criteria:
+
+  [] - User is taken to their recipe index after successful sign-in
 } do
-  scenario 'specify valid credentials' do
-    user = FactoryGirl.create(:user)
+  context 'valid credentials' do
 
-    visit new_user_session_path
+    before(:each) do
+      user = FactoryGirl.create(:user)
 
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
+      visit new_user_session_path
 
-    click_button 'Log in'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
 
-    expect(page).to have_content('Signed in successfully')
-    expect(page).to have_content('Sign Out')
+      click_button 'Log in'
+    end
+
+    scenario 'user recieves confirmation' do
+      expect(page).to have_content('Signed in successfully')
+      expect(page).to have_content('Sign Out')
+    end
+
+    scenario 'user is taken to their index' do
+      pending("Waiting for recipe index to be implemented")
+      expect(current_path).to eq(recipes_path)
+    end
   end
 
   scenario 'specify invalid credentials' do
