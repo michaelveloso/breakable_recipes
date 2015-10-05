db7631b76c64a5b28ef420da557a500c730a2961
michaelveloso <mjv.audio@gmail.com>
2015-10-05 11:12:45 -0400

    moderator can delete categories

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :ingredients, except: [:show]
 
-  resources :categories, only: [:index, :create, :edit, :update]
+  resources :categories, except: [:show, :new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
