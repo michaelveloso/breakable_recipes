81db828e3c49973ed4b824541927aa58b61c2f55
michaelveloso <mjv.audio@gmail.com>
2015-10-05 10:16:51 -0400

    moderator can edit categories

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :ingredients, except: [:show]
 
-  resources :categories, only: [:index, :create]
+  resources :categories, only: [:index, :create, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
