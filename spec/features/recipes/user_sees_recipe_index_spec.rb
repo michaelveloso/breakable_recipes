99832aeff8da1439e4888613e95ac9411026bf3e
michaelveloso <mjv.audio@gmail.com>
2015-10-07 11:15:56 -0400

    hound

--- spec/features/recipes/user_sees_recipe_index_spec.rb
+++ spec/features/recipes/user_sees_recipe_index_spec.rb
@@ -40,7 +40,6 @@ feature 'user sees recipe index', %{
   end
 
   scenario 'recipes are links to their show pages' do
-    save_and_open_page
     click_link(@recipe.name)
 
     expect(current_path).to eq(recipe_path(@recipe))
